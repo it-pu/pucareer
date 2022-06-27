6320b3943bdff59c250efe470bf252f16a0085c3
Fajar Ramadhan Putra Santoso <fajar.santoso@podomorouniversity.ac.id>
2022-06-27 14:26:11 +0700

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -1,6 +1,6 @@
 <?php
 	class SConfig{
-		var $_site_url = "http://localhost/pucareer";
+		var $_site_url = "http://localhost:8080/pucareer";
 		var $_document_root = "C:/xampp/htdocs/pucareer";
 		var $_host_name = "localhost";
 		var $_site_name = "cliv2";
@@ -8,5 +8,5 @@
 		var $_database_user = "root";
 		var $_database_password = "";
 		var $_cms_name = "";
-		var $_host_server = "http://localhost/pucareer";
+		var $_host_server = "http://localhost:8080/pucareer";
 	}
